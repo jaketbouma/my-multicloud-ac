b9a934a923645c6979cabb9d119adbb536f56171
Jake Bouma <jake.t.bouma@gmail.com>
2025-02-13 16:52:30 +0100

    cleaned up access to idc and s3

--- main.tf
+++ main.tf
@@ -47,16 +47,91 @@ resource "aws_ssoadmin_managed_policy_attachment" "sandboxes" {
   permission_set_arn = aws_ssoadmin_permission_set.sandboxes.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+resource "aws_ssoadmin_permission_set_inline_policy" "sandboxes" {
+  instance_arn       = var.aws_sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.sandboxes.arn
+  inline_policy      = data.aws_iam_policy_document.sandboxes_assume_role_in_root.json
+}
+data "aws_iam_policy_document" "sandboxes_assume_role_in_root" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    resources = [
+      aws_iam_role.sandbox_access.arn
+    ]
+  }
+}
+
+
+
+#
+# Create a policy set to give access to the identity store
+
+data "aws_iam_policy_document" "sandbox_to_root_inline" {
+  statement {
+    actions = [
+      "identitystore:CreateGroup",
+      "identitystore:DeleteGroup",
+      "identitystore:CreateGroupMembership",
+      "identitystore:UpdateGroup",
+      "identitystore:DescribeGroup"
+    ]
+    resources = [
+      "arn:aws:identitystore::${var.aws_root_account_id}:identitystore/${var.aws_sso_instance_identity_store_id}",
+      "arn:aws:identitystore:::group/*"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:*"
+    ]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.terraform_statefiles.id}/*"]
+  }
+  statement {
+    actions = [
+      "dynamodb:*"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/terraform.statelock.*"]
+  }
+}
+data "aws_iam_policy_document" "sandbox_to_root_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        for account in aws_organizations_account.sandbox_accounts : "arn:aws:iam::${account.id}:root"
+      ]
+    }
+  }
+}
+resource "aws_iam_role" "sandbox_access" {
+  name               = "SandboxAccess"
+  description        = "Allows sandbox accounts to access IdC and statefiles in the root account"
+  assume_role_policy = data.aws_iam_policy_document.sandbox_to_root_assume_role.json
+  inline_policy {
+    name   = "SandboxToRootInlinePolicy"
+    policy = data.aws_iam_policy_document.sandbox_to_root_inline.json
+  }
+}
+
+
+#
+#
 resource "aws_identitystore_group" "terraform_developers" {
   identity_store_id = var.aws_sso_instance_identity_store_id
   display_name      = "terraform_developers"
   description       = "Users who can administer terraform sandboxes"
 }
 resource "aws_identitystore_group_membership" "terraform_developers_membership" {
-  for_each = toset(var.aws_idc_admin_user_ids)
+  for_each          = toset(var.aws_idc_admin_user_ids)
   identity_store_id = var.aws_sso_instance_identity_store_id
-  group_id = aws_identitystore_group.terraform_developers.group_id
-  member_id = each.key
+  group_id          = aws_identitystore_group.terraform_developers.group_id
+  member_id         = each.key
 }
 
 module "split_admin_email" {
@@ -67,7 +142,7 @@ module "split_admin_email" {
 #
 # Create a state backend for every aws sandbox project
 resource "aws_organizations_account" "sandbox_accounts" {
-  for_each = local.aws_accounts
+  for_each          = local.aws_accounts
   name              = each.key
   email             = each.value.email
   role_name         = "Admin"
@@ -79,15 +154,15 @@ resource "aws_organizations_account" "sandbox_accounts" {
 }
 resource "aws_s3_object" "sandbox_statefiles" {
   for_each = local.aws_accounts
-  bucket = aws_s3_bucket.terraform_statefiles.id
-  key    = "${each.key}/"
+  bucket   = aws_s3_bucket.terraform_statefiles.id
+  key      = "${each.key}/"
   tags = {
     "project"   = each.value.project
     "component" = "terraform"
   }
 }
 resource "aws_dynamodb_table" "sandbox_statelocks" {
-  for_each = local.aws_accounts
+  for_each       = local.aws_accounts
   name           = "terraform.statelock.${each.key}"
   read_capacity  = 5
   write_capacity = 5
@@ -104,7 +179,7 @@ resource "aws_dynamodb_table" "sandbox_statelocks" {
 
 # Grant the terraform_developers group admin access to all of the sandboxes
 resource "aws_ssoadmin_account_assignment" "sandbox_access" {
-  for_each = aws_organizations_account.sandbox_accounts
+  for_each           = aws_organizations_account.sandbox_accounts
   instance_arn       = var.aws_sso_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.sandboxes.arn
 
--- project_configurations.tf
+++ project_configurations.tf
@@ -3,12 +3,12 @@
 locals {
   aws_accounts = {
     "bookstore" = {
-      project  = "bookstore"
-      email = "${module.split_admin_email.parts.name}+bookstore@${module.split_admin_email.parts.domain}"
+      project = "bookstore"
+      email   = "${module.split_admin_email.parts.name}+bookstore@${module.split_admin_email.parts.domain}"
     }
     "platform" = {
-      project  = "platform"
-      email = "${module.split_admin_email.parts.name}+platform@${module.split_admin_email.parts.domain}"
+      project = "platform"
+      email   = "${module.split_admin_email.parts.name}+platform@${module.split_admin_email.parts.domain}"
     }
   }
 }
